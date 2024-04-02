09b268637b3f012010a9825b3faf886675a9811d
lil-fe <tranzocchi.1891909@studenti.uniroma1.it>
2024-04-02 18:39:26 +0200

    fix quantum, adjust scheduler function

--- sched_sim.c
+++ sched_sim.c
@@ -12,46 +12,9 @@ typedef struct {
 void schedSJF(FakeOS* os, void *args_, int cpu_index) {
     SchedSJFArgs* args = (SchedSJFArgs*) args_;
     FakePCB* shortest_job = findShortestJob(&os->ready);
-    if (shortest_job->actual_burst == 0) return;
-   
-    FakePCB* running = os->cpus[cpu_index].running;
-    if (running) {
-        ProcessEvent* current_burst = (ProcessEvent*) running->events.first;
-        //printf("\t[debug] current_burst->duration = %d\n", current_burst->duration);
-        // compare shortest_job->actual_burst to the duration of the currently running event 
-        if (shortest_job->actual_burst < current_burst->duration) {    
-           /* perform preemption APPROACH 1
-            * 1. Create a new CPU event with duration equals to current_burst->duration
-            * 2. Set current_burst->duration=0 so that the preempted process 
-            *    will be pushed back into the ready list
-            * 3. Prepend the new CPU event to the events list of the preempted process.
-            *    This step has to be performed after the popping of the old event from the list,
-            *    otherwise the popped event will be the new one. */
-            /*ProcessEvent* tmp = (ProcessEvent*) malloc(sizeof(ProcessEvent));
-            tmp->list.prev = tmp->list.next = 0;
-            tmp->type = CPU;
-            tmp->duration = current_burst->duration;
-            printf("\t[debug]tmp->duration = %d\n", tmp->duration);
-            current_burst->duration = 0;
-            printf("\t[debug]current_burst->duration after preemption = %d of [pid %d]\n",
-                    current_burst->duration, running->pid);
-            // THIS HAS TO BE PERFORMED AFTER THE OLD PROCESS HAS BEEN POPPED
-            List_pushFront(&running->events, (ListItem*) tmp);*/
-            
-            /* perform preemption APPROACH 2 */
-            running->predicted_burst = current_burst->duration;
-            List_pushBack(&os->ready, (ListItem*) running);
-            printf("\t\t\t[CPU %d] assigned to [pid %d], [pid %d] moved to ready \n",
-                    cpu_index, shortest_job->pid, running->pid);
-        } 
-    }
 
-    os->cpus[cpu_index].running = shortest_job;
-    List_detach(&os->ready, (ListItem*) shortest_job);
+    // handle the event when it exceeds the quantum
     ProcessEvent* e = (ProcessEvent*)shortest_job->events.first;
-    /* if e->duration > quantum, then prepend to the events list of this process 
-     * a CPU event of duration quantum,
-     * then alter the duration of the old event subtracting quantum */
     if (e->duration > args->quantum) {
         ProcessEvent* qe = (ProcessEvent*) malloc(sizeof(ProcessEvent));
         qe->list.prev = qe->list.next = 0;
@@ -60,6 +23,27 @@ void schedSJF(FakeOS* os, void *args_, int cpu_index) {
         e->duration -= args->quantum;
         List_pushFront(&shortest_job->events, (ListItem*) qe);
     }
+
+    FakePCB* running = os->cpus[cpu_index].running;
+    if (running) {
+        ProcessEvent* current_burst = (ProcessEvent*) running->events.first;
+        /* if a ready job shorter than the current running CPU burst is found,
+         * then perform preemption */
+        if (shortest_job->actual_burst < current_burst->duration
+                && shortest_job->actual_burst > 0) {
+            List_pushBack(&os->ready, (ListItem*) running);
+            os->cpus[cpu_index].running = shortest_job;
+            List_detach(&os->ready, (ListItem*) shortest_job);
+            printf("\t\t\t[CPU %d] assigned to [pid %d], [pid %d] moved to ready \n",
+                    cpu_index, shortest_job->pid, running->pid);
+        }
+        /* could not find any shorter job */
+        return;
+    }
+    
+    // if no processes are running on this CPU, just assign it to the shortest job.
+    os->cpus[cpu_index].running = shortest_job;
+    List_detach(&os->ready, (ListItem*) shortest_job);
 }
 
 int main(int argc, char **argv) {
